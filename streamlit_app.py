7b71724b14ef049d70f58f49cfc24d12c523158e
PietroZambetti <pietrozambetti3@gmail.com>
2025-03-27 09:57:39 +0000

    primocommit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new app for DV4S!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Vediamo come cazzo funziona! [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+value = st.slider('Select a value', 0, 100, 30)
